2f36f1800386a2aeb399d1dc731f1b0e3d7b480b
dinghy <88888888>
2019-10-09 15:47:15 +0800

    添加

---
+++ doc/JVM参数.md
@@ -0,0 +1,3 @@
+#jvm一些参数
+(1)java -XX:+PrintCommandLineFlags -version 查看当前使用的是哪种垃圾收集器
+    
---
+++ src/threadpool/ThreadPoolDemo.java
@@ -0,0 +1,20 @@
+package threadpool;
+
+
+/**
+ * @author dinghy
+ * @date 2019/10/9 12:39
+ *
+ * <p>
+ *     FixedThreadPool  LinkedBlockingQueue
+ *     SingleThreadExecutor   LinkedBlockingQueue
+ *     CachedThreadPool    SynchronousQueue
+ *
+ * </p>
+ */
+public class ThreadPoolDemo{
+    public static void main(String[] args) {
+
+    }
+
+}
