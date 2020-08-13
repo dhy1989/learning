c6ed76486042e58c71c1d7e12bdb25aa119be3ed
dinghy <88888888>
2020-08-13 13:28:28 +0800

    quartz

---
+++ src/quarz/HelloJob.java
@@ -0,0 +1,16 @@
+package quarz;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+/**
+ * @author dinghy
+ * @date 2020/8/13 11:42
+ */
+public class HelloJob implements Job {
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        System.err.println("Hello!  HelloJob is executing.");
+    }
+}
---
+++ src/quarz/QuartzDemo.java
@@ -0,0 +1,41 @@
+package quarz;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author dinghy
+ * @date 2020/8/13 11:12
+ */
+public class QuartzDemo {
+    public static void main(String[] args) throws SchedulerException, InterruptedException {
+        // 1、创建调度器Scheduler
+        SchedulerFactory schedulerFactory = new StdSchedulerFactory();
+        Scheduler scheduler = schedulerFactory.getScheduler();
+        // 2、创建JobDetail实例，并与PrintWordsJob类绑定(Job执行内容)
+        JobDetail jobDetail = JobBuilder.newJob(HelloJob.class)
+                .withIdentity("job1", "group1").build();
+        // 3、构建Trigger实例,每隔1s执行一次
+        Trigger trigger = TriggerBuilder.newTrigger().withIdentity("trigger1", "triggerGroup1")
+                .startNow()//立即生效
+                .withSchedule(SimpleScheduleBuilder.simpleSchedule()
+                        .withIntervalInSeconds(1)//每隔1s执行一次
+                        .repeatForever()).build();//一直执行
+
+        //4、执行
+        scheduler.scheduleJob(jobDetail, trigger);
+        System.out.println("--------scheduler start ! ------------");
+        scheduler.start();
+
+        //睡眠
+        TimeUnit.MINUTES.sleep(1);
+        scheduler.shutdown();
+        System.out.println("--------scheduler shutdown ! ------------");
+
+    }
+}
