3bda1446b2822c3f3a88a0c970274cf5c7d2b88d
dinghy <88888888>
2021-01-05 17:58:53 +0800

    Hmac工具

---
+++ src/encrypt/HmacUtil.java
@@ -0,0 +1,22 @@
+package encrypt;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+/**
+ * @author dinghy
+ * @date 2021/1/5 17:58
+ */
+public class HmacUtil {
+    public static String HMACSHA256(String data, String key) throws Exception {
+        Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+        SecretKeySpec secret_key = new SecretKeySpec(key.getBytes("UTF-8"), "HmacSHA256");
+        sha256_HMAC.init(secret_key);
+        byte[] array = sha256_HMAC.doFinal(data.getBytes("UTF-8"));
+        StringBuilder sb = new StringBuilder();
+        for (byte item : array) {
+            sb.append(Integer.toHexString((item & 0xFF) | 0x100).substring(1, 3));
+        }
+        return sb.toString().toUpperCase();
+    }
+}
