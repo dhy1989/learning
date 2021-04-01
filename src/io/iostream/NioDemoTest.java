83aec1e00df967c2608dac62d31665a09e91ed3e
dinghy <88888888>
2021-04-01 11:28:07 +0800

    nio里边buffer

--- src/io/iostream/NioDemoTest.java
+++ src/io/iostream/NioDemoTest.java
@@ -1,10 +1,8 @@
 package io.iostream;
 
-import com.sun.xml.internal.stream.util.BufferAllocator;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.nio.Buffer;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.charset.StandardCharsets;
